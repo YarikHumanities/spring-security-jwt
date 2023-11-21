867bb862ba7495f9aeae63718590b4e5ea20f398
Yarik <yaroslav.matsapura@gmail.com>
2023-11-21 18:48:48 +0200

    add UserDetailsService implementation to be created as bean in application configuration

---
+++ src/main/java/com/example/springsecurity/config/ApplicationConfig.java
@@ -0,0 +1,25 @@
+package com.example.springsecurity.config;
+
+import com.example.springsecurity.user.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+@Configuration
+@RequiredArgsConstructor
+public class ApplicationConfig {
+    private final UserRepository repository;
+
+    @Bean
+    public UserDetailsService userDetailsService(){
+        return new UserDetailsService() {
+            @Override
+            public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+                return repository.findByEmail(username).orElseThrow(()->new UsernameNotFoundException("User not found"));
+            }
+        };
+    }
+}
