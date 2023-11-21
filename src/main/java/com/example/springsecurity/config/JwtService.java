88ec55a4c580476710f09c74f890a1c0150eb807
Yarik <yaroslav.matsapura@gmail.com>
2023-11-21 17:36:27 +0200

    add final realization for JwtService methods

--- src/main/java/com/example/springsecurity/config/JwtService.java
+++ src/main/java/com/example/springsecurity/config/JwtService.java
@@ -1,10 +1,30 @@
 package com.example.springsecurity.config;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Service;
+
+import java.security.Key;
 
 @Service
 public class JwtService {
+    private static final String SECRET_KEY = "93FECF624828BB797DBCDC6CCAA8D";
     public String extractUsername(String token) {
         return null;
+    }
+    private Claims extractAllClaims(String token){
+        return Jwts
+                .parserBuilder()
+                .setSigningKey(getSingInKey())
+                .build()
+                .parseClaimsJwt(token)
+                .getBody();
+    }
+
+    private Key getSingInKey() {
+        byte[] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
+        return Keys.hmacShaKeyFor(keyBytes);
     }
 }
