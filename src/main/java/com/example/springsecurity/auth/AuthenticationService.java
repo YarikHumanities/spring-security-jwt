ebc1e9aef879ad9d8c07d938174b632080a9b755
Yarik <yaroslav.matsapura@gmail.com>
2023-11-22 00:11:04 +0200

    add authenticate method in AuthenticationService implementation

--- src/main/java/com/example/springsecurity/auth/AuthenticationService.java
+++ src/main/java/com/example/springsecurity/auth/AuthenticationService.java
@@ -5,6 +5,8 @@ import com.example.springsecurity.user.Role;
 import com.example.springsecurity.user.User;
 import com.example.springsecurity.user.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -14,6 +16,7 @@ public class AuthenticationService {
     private final UserRepository repository;
     private final PasswordEncoder passwordEncoder;
     private final JwtService jwtService;
+    private final AuthenticationManager authenticationManager;
     public AuthenticationResponse register(RegisterRequest request) {
         var user = new User();
         user.setFirstName(request.getFirstName());
@@ -32,6 +35,20 @@ public class AuthenticationService {
                 .build();
     }
     public AuthenticationResponse authenticate(AuthenticationRequest request) {
-        return null;
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        request.getEmail(),
+                        request.getPassword()
+                )
+        );
+        var user = repository.findByEmail(request.getEmail())
+                .orElseThrow();
+
+        var jwtToken = jwtService.generateToken(user);
+
+        return AuthenticationResponse
+                .builder()
+                .token(jwtToken)
+                .build();
     }
 }
