ea1e61423f09471d86ac57a968a6b8333a39800e
Yarik <yaroslav.matsapura@gmail.com>
2023-11-21 14:25:47 +0200

    add implemented methods realisation for User class

--- src/main/java/com/example/springsecurity/user/User.java
+++ src/main/java/com/example/springsecurity/user/User.java
@@ -6,9 +6,11 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
+import java.util.List;
 
 @Data
 @Builder
@@ -24,37 +26,39 @@ public class User implements UserDetails {
     private String firstName;
     private String lastName;
     private String email;
+
+    //getPassword() is already implemented because we have field "password". In other cases getPassword() must be implemented manualy
     private String password;
     @Enumerated(EnumType.STRING)
     private Role role;
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return null;
+        return List.of(new SimpleGrantedAuthority(role.name()));
     }
 
     @Override
     public String getUsername() {
-        return null;
+        return email;
     }
 
     @Override
     public boolean isAccountNonExpired() {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isAccountNonLocked() {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isCredentialsNonExpired() {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isEnabled() {
-        return false;
+        return true;
     }
 }
