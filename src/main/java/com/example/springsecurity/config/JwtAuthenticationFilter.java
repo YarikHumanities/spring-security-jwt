6f9cb3a0aa186ded66e30bf8d1141cb82a8fdbac
Yarik <yaroslav.matsapura@gmail.com>
2023-11-21 16:53:15 +0200

    add request header validation for JwtFilter

--- src/main/java/com/example/springsecurity/config/JwtAuthenticationFilter.java
+++ src/main/java/com/example/springsecurity/config/JwtAuthenticationFilter.java
@@ -4,13 +4,30 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 
+//To make a manage Spring bean from a Class
+@Component
+
+//Constructor for final fields
+@RequiredArgsConstructor
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
     @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-
+    protected void doFilterInternal(
+            @NonNull HttpServletRequest request,
+            @NonNull HttpServletResponse response,
+            @NonNull FilterChain filterChain
+    ) throws ServletException, IOException {
+        final String authHeader = request.getHeader("Authorization");
+        final String iwt;
+        if(authHeader == null || !authHeader.startsWith("Bearer ")){
+            filterChain.doFilter(request, response);
+            return;
+        }
     }
 }
